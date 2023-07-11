e02cff110eb7b62945af665bc0e6ad66177568a1
mrcrypsie <richardkeke15@gmail.com>
2023-07-11 02:35:00 +0100

    Mise à jour du projet

--- app_host/location/views.py
+++ app_host/location/views.py
@@ -2,27 +2,10 @@ from django.shortcuts import render
 from store.models import Product
 
 def home(request):
-    return render(request, 'index.html')
-
-def about(request):
-    return render(request, 'about-us.html')
-
-def contact(request):
-    return render(request, 'contact.html')
-
-def paiement(request):
-    return render(request, 'inscription.html')
-
-def news(request):
     products = Product.objects.all().filter(is_available=True)
 
     context = {
         'products':products,
     }
-    return render(request, 'news.html', context)
+    return render(request, 'home.html', context)
 
-def read_more(request):
-    return render(request, 'read_more.html')
-
-def services(request):
-    return render(request, 'services.html')
